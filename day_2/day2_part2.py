14cd412c7d8f4dd8b30e7637fdfda53bf86a9080
Liviu <liviuarsenescu@gmail.com>
2024-12-04 11:31:33 +0100

    wip(day2 part2): copy part 1 in part2

--- day_2/day2_part2.py
+++ day_2/day2_part2.py
@@ -1,23 +1,15 @@
 def check_row(row: list):
-    for i in range(1, len(row)):
-        diff = abs(row[i - 1] - row[i])
-        if diff < 1 or diff > 3:
-            copy_row = row.copy()
-            copy_row.pop(i)
-            print(copy_row)
-            if not check_row(copy_row):
-                return False
-
     result_inc = True
     result_dec = True
 
-    # Check for inc
     for i in range(1, len(row)):
+        diff = abs(row[i - 1] - row[i])
+        if diff < 1 or diff > 3:
+            return False
+
         if row[i - 1] > row[i]:
             result_inc = False
 
-    # Check for dec
-    for i in range(1, len(row)):
         if row[i - 1] < row[i]:
             result_dec = False
 
@@ -38,7 +30,7 @@ if __name__ == '__main__':
     safe_reports = 0
     for i in range(len(matrix)):
         result = check_row(matrix[i])
-        print(f"Row {i} -> {check_row(matrix[i])}")
+        # print(f"Row {i} -> {check_row(matrix[i])}")
         if result:
             safe_reports += 1
 
