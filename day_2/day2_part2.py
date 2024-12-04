5dd864d8b866e0342ddfa9f129a94a7e2165b487
Liviu <liviuarsenescu@gmail.com>
2024-12-04 11:26:00 +0100

    fix(day2_part1): merge the 3 fors in 1

--- day_2/day2_part2.py
+++ day_2/day2_part2.py
@@ -1,15 +1,23 @@
 def check_row(row: list):
-    result_inc = True
-    result_dec = True
-
     for i in range(1, len(row)):
         diff = abs(row[i - 1] - row[i])
         if diff < 1 or diff > 3:
-            return False
+            copy_row = row.copy()
+            copy_row.pop(i)
+            print(copy_row)
+            if not check_row(copy_row):
+                return False
 
+    result_inc = True
+    result_dec = True
+
+    # Check for inc
+    for i in range(1, len(row)):
         if row[i - 1] > row[i]:
             result_inc = False
 
+    # Check for dec
+    for i in range(1, len(row)):
         if row[i - 1] < row[i]:
             result_dec = False
 
@@ -30,7 +38,7 @@ if __name__ == '__main__':
     safe_reports = 0
     for i in range(len(matrix)):
         result = check_row(matrix[i])
-        # print(f"Row {i} -> {check_row(matrix[i])}")
+        print(f"Row {i} -> {check_row(matrix[i])}")
         if result:
             safe_reports += 1
 
